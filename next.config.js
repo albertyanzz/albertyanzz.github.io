0e2c496d6b614b20e0b3895013334151ef2a9e66
Albert Yan <albertzy@usc.edu>
2022-03-26 21:05:48 -0700

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== "production";
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  assetPrefix: !debug ? "/albertyanzz/" : "",
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
