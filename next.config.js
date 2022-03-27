c9b540e67ad85e8a99e868526183bdb733fc922e
Albert Yan <albertzy@usc.edu>
2022-03-26 21:30:45 -0700

    removed asset prefix

--- next.config.js
+++ next.config.js
@@ -1,10 +1,7 @@
 /** @type {import('next').NextConfig} */
 
-const debug = process.env.NODE_ENV !== "production";
-
 const nextConfig = {
   reactStrictMode: true,
-  assetPrefix: !debug ? "/albertyanzz/" : "",
 };
 
 module.exports = nextConfig;
